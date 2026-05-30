fc655a3bc250e36d9d86f5f60008bab274f0014e
DavidTorres-sys <david.torresg@udea.edu.co>
2026-05-30 03:08:44 +0200

    allow cors

--- src/main/java/com/ebv14/backend/controller/PresupuestoController.java
+++ src/main/java/com/ebv14/backend/controller/PresupuestoController.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/presupuesto")
+@CrossOrigin(origins = "https://financial-management-frontend-rho.vercel.app")
 public class PresupuestoController {
 
     private final UsuarioRepository usuarioRepository;
