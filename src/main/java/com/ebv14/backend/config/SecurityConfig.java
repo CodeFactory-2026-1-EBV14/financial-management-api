3f488331ceea883ba4af7ec4ba43d12a7a563a53
David1863-sys <75543471+DavidTorres-sys@users.noreply.github.com>
2026-06-03 02:38:40 +0200

    Update CORS configuration to allow new origins

--- src/main/java/com/ebv14/backend/config/SecurityConfig.java
+++ src/main/java/com/ebv14/backend/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://localhost:8081"
+                "http://localhost:8080",
+                "financial-management-frontend-rho.vercel.app"                 
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
